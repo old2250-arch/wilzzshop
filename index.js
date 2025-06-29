ba3b6f5f2edd70a6ff7907b31c4dd5a7eff1948a
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-06-29 09:41:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1400,8 +1400,8 @@ app.get("/data/users", requireAdmin, async (req, res) => {
   }
 });
 
-app.post('/admin/verify-user', requireAdmin, async (req, res) => {
-  const { username } = req.body;
+app.get('/admin/verify-user', requireAdmin, async (req, res) => {
+  const { username } = req.query;
   if (!username) {
     return res.status(400).json({
       success: false,
