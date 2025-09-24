e8a1ec00c9ec40dba002578f4e896d541353cc16
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-09-24 09:54:17 +0700

    Update admin.js

--- endpoint/admin.js
+++ endpoint/admin.js
@@ -29,9 +29,19 @@ router.get("/profile", async (req, res) => {
     const response = await cloudscraper.post("https://atlantich2h.com/get_profile", {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Origin": "https://atlantich2h.com",
+        "Referer": "https://atlantich2h.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+        "Connection": "keep-alive"
       },
-      body
+      body,
+      gzip: true,
+      resolveWithFullResponse: false,
+      simple: true
     })
     const extData = JSON.parse(response)
     const result = {
